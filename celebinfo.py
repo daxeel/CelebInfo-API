355ec6dc4702811de164d57805e12bba4dbf6c3d
Daxeel Soni <daxeel_soni4444@yahoo.com>
2015-12-28 13:21:17 +0530

    [MAJOR UPDATE] : Search by celebrity name
    Now you can pass celebrity name to get his information

--- celebinfo.py
+++ celebinfo.py
@@ -87,5 +87,54 @@ def custom(filter_type):
 	else:
 		return {"error": "Invalid API syntax"}
 
+@app.route('/api/name/<celeb_name>')
+def name(celeb_name):
+	URL = "http://www.famousbirthdays.com/names/" + celeb_name + ".html"
+	HTML = requests.get(URL).content
+	SOUP = BeautifulSoup(HTML, "html.parser")
+
+	ALL_LINKS = SOUP.find_all('a', 'celeb')
+
+	# Variables to store date
+	response = {}
+	data = []
+	
+	# Gettinge data from each links
+	for each in ALL_LINKS:
+
+		URL = each['href']
+		HTML = requests.get(URL).content
+		SOUP = BeautifulSoup(HTML, "html.parser")
+
+		try:name = SOUP.find_all('h2')[1].string
+		except:name = "Not Found"
+
+		try:photo_url = SOUP.figure.a.img['src']
+		except:photo_url = "Not Found"
+		
+		try:occupation = SOUP.findAll("strong", { "class" : "uppercase" })[0].a.font.span.string
+		except:occupation = "Not Found"
+
+		try:birthday = SOUP.findAll("strong", { "class" : "overflow" })[0].time.a.b.font.string
+		except:birthday = "Not Found"
+
+		try:birth_year = SOUP.findAll("strong", { "class" : "overflow" })[0].time.find_all('a')[1].b.font.string
+		except:birth_year = "Not Found"
+
+		try:birth_place = SOUP.findAll("strong", { "class" : "overflow" })[1].a.b.font.string
+		except:birth_place = "Not Found"
+
+		try:age = int((SOUP.findAll("strong", { "class" : "overflow" })[2].b.font.a.font.string).split()[0])
+		except:age = "Not Found"
+
+		try:birth_sign = SOUP.findAll("strong", { "class" : "overflow" })[3].a.b.font.string
+		except:birth_sign = "Not Found"
+
+		data.append({"name": name, "photo_url": photo_url, "occupation": occupation, "birthday": birthday, "birth_year": birth_year, "birth_place": birth_place, "age": age, "birth_sign": birth_sign})
+
+	# Return json response
+	response['data'] = data
+	return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
 if __name__ == '__main__':
     app.run()
